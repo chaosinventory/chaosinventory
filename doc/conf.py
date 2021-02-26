1518b8e6fc18016140c64da6f91b9889f38cc316
Marvin Gaube <dev@marvingaube.de>
2021-02-26 20:17:51 +0100

    Temporarily disable django builds

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,8 @@ import django
 sys.path.insert(0, os.path.abspath('../src'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chaosinventory.settings")
-django.setup()
+# FIXME: The django setup is broken at the moment. Uncomment after django is stable.
+# django.setup()
 
 
 # -- Project information -----------------------------------------------------
