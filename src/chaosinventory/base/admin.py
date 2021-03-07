72f9dbf7f1c52e21ffc6111cad26fd1525c9fd4f
clerie <git@clerie.de>
2021-03-07 17:10:47 +0100

    Fix code formating

--- src/chaosinventory/base/admin.py
+++ src/chaosinventory/base/admin.py
@@ -1,158 +1,175 @@
 from django.contrib import admin  # noqa
 
 from .models import (
-    Tag, DataType, EntityData, LocationData, ProductData, ProductInventoryId,
-    ItemData, ItemInventoryId, Entity, Location, Product, Item, InventoryIdSchema,
-    Overlay, OverlayItem,
+    DataType, Entity, EntityData, InventoryIdSchema, Item, ItemData,
+    ItemInventoryId, Location, LocationData, Overlay, OverlayItem, Product,
+    ProductData, ProductInventoryId, Tag,
 )
+
 
 class CommonInline(admin.TabularInline):
     extra = 0
 
+
 class CommonDataInline(CommonInline):
-    fields = ( 'type', 'value', )
+    fields = ('type', 'value',)
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {
-            'fields': ( 'name', ),
+            'fields': ('name',),
         }),
         ("Properties", {
-            'fields': ( 'parent', ),
+            'fields': ('parent',),
         }),
     )
+
 
 @admin.register(DataType)
 class DataTypeAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {
-            'fields': ( 'name', 'note', ),
+            'fields': ('name', 'note',),
         }),
     )
+
 
 class EntityDataInline(CommonDataInline):
     model = EntityData
 
+
 class LocationDataInline(CommonDataInline):
     model = LocationData
 
+
 class ProductDataInline(CommonDataInline):
     model = ProductData
+
 
 @admin.register(ProductInventoryId)
 class ProductInventoryIdAdmin(admin.ModelAdmin):
     pass
 
+
 class ItemDataInline(CommonDataInline):
     model = ItemData
+
 
 @admin.register(ItemInventoryId)
 class ItemInventoryIdAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Entity)
 class EntityAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {
-            'fields': ( 'name', 'note', ),
+            'fields': ('name', 'note',),
         }),
         ("Ownership", {
-            'fields': ( 'part_of', ),
+            'fields': ('part_of',),
         }),
         ("Tags", {
-            'fields': ( 'tags', ),
+            'fields': ('tags',),
         }),
     )
     inlines = [
         EntityDataInline,
     ]
 
+
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {
-            'fields': ( 'name', 'note', ),
+            'fields': ('name', 'note',),
         }),
         ("Location", {
-            'fields': ( 'in_location', ),
+            'fields': ('in_location',),
         }),
         ("Ownership", {
-            'fields': ( 'belongs_to', ),
+            'fields': ('belongs_to',),
         }),
         ("Tags", {
-            'fields': ( 'tags', ),
+            'fields': ('tags',),
         }),
     )
     inlines = [
         LocationDataInline,
     ]
 
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {
-            'fields': ( 'name', 'note', ),
+            'fields': ('name', 'note',),
         }),
         ("Inventory IDs", {
-            'fields': ( 'inventory_id', ),
+            'fields': ('inventory_id',),
         }),
         ("Tags", {
-            'fields': ( 'tags', ),
+            'fields': ('tags',),
         }),
     )
     inlines = [
         ProductDataInline,
     ]
 
+
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {
-            'fields': ( 'name', 'note', ),
+            'fields': ('name', 'note',),
         }),
         ("Properties", {
-            'fields': ( 'product', 'amount', ),
+            'fields': ('product', 'amount',),
         }),
         ("Location", {
-            'fields': ( 'target_location', 'actual_location', 'target_item', 'actual_item', ),
+            'fields': ('target_location', 'actual_location', 'target_item', 'actual_item',),
         }),
         ("Ownership", {
-            'fields': ( 'belongs_to', ),
+            'fields': ('belongs_to',),
         }),
         ("Inventory IDs", {
-            'fields': ( 'inventory_id', ),
+            'fields': ('inventory_id',),
         }),
         ("Tags", {
-            'fields': ( 'tags', ),
+            'fields': ('tags',),
         }),
     )
     inlines = [
         ItemDataInline,
     ]
 
+
 @admin.register(InventoryIdSchema)
 class InventoryIdSchemaAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {
-            'fields': ( 'name', 'note', ),
+            'fields': ('name', 'note',),
         }),
     )
 
+
 class OverlayItemInline(CommonInline):
     model = OverlayItem
+
 
 @admin.register(Overlay)
 class OverlayAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {
-            'fields': ( 'name', 'note', ),
+            'fields': ('name', 'note',),
         }),
         ("Status", {
-            'fields': ( 'active', ),
+            'fields': ('active',),
         }),
         ("Properties", {
-            'fields': ( 'parent', ),
+            'fields': ('parent',),
         }),
     )
     inlines = [
