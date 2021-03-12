97c14d83518185034f1047a78dd10a18b12e6928
clerie <clerie@users.noreply.github.com>
2021-03-12 19:46:03 +0100

    Bootstrapping Admin Interface (#19)
    * Init admin.py
    
    * Add fieldset for Item
    
    * Add fieldset for ItemData
    
    * Add fieldsets for Product and ProductData
    
    * Add fieldsets for Location and LocationData
    
    * Add fieldsets for Entity and EntityData
    
    * Add fieldsets for Overlay and OverlayItem
    
    * Add explicit fieldset for Tag, DataType and InventoryIdSchema
    
    * Fix code formating
    
    * Patch AdminUI to support new Inventory ID schema

--- src/chaosinventory/base/admin.py
+++ src/chaosinventory/base/admin.py
@@ -1,3 +1,171 @@
 from django.contrib import admin  # noqa
 
-# Register your models here.
+from .models import (
+    DataType, Entity, EntityData, InventoryIdSchema, Item, ItemData,
+    ItemInventoryId, Location, LocationData, Overlay, OverlayItem, Product,
+    ProductData, ProductInventoryId, Tag,
+)
+
+
+class CommonInline(admin.TabularInline):
+    extra = 0
+
+
+class CommonDataInline(CommonInline):
+    fields = ('type', 'value',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('name',),
+        }),
+        ("Properties", {
+            'fields': ('parent',),
+        }),
+    )
+
+
+@admin.register(DataType)
+class DataTypeAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'note',),
+        }),
+    )
+
+
+class EntityDataInline(CommonDataInline):
+    model = EntityData
+
+
+class LocationDataInline(CommonDataInline):
+    model = LocationData
+
+
+class ProductDataInline(CommonDataInline):
+    model = ProductData
+
+
+class ProductInventoryIdInline(CommonInline):
+    model = ProductInventoryId
+
+
+class ItemDataInline(CommonDataInline):
+    model = ItemData
+
+
+class ItemInventoryIdInline(CommonInline):
+    model = ItemInventoryId
+
+
+@admin.register(Entity)
+class EntityAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'note',),
+        }),
+        ("Ownership", {
+            'fields': ('part_of',),
+        }),
+        ("Tags", {
+            'fields': ('tags',),
+        }),
+    )
+    inlines = [
+        EntityDataInline,
+    ]
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'note',),
+        }),
+        ("Location", {
+            'fields': ('in_location',),
+        }),
+        ("Ownership", {
+            'fields': ('belongs_to',),
+        }),
+        ("Tags", {
+            'fields': ('tags',),
+        }),
+    )
+    inlines = [
+        LocationDataInline,
+    ]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'note',),
+        }),
+        ("Tags", {
+            'fields': ('tags',),
+        }),
+    )
+    inlines = [
+        ProductInventoryIdInline,
+        ProductDataInline,
+    ]
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'note',),
+        }),
+        ("Properties", {
+            'fields': ('product', 'amount',),
+        }),
+        ("Location", {
+            'fields': ('target_location', 'actual_location', 'target_item', 'actual_item',),
+        }),
+        ("Ownership", {
+            'fields': ('belongs_to',),
+        }),
+        ("Tags", {
+            'fields': ('tags',),
+        }),
+    )
+    inlines = [
+        ItemInventoryIdInline,
+        ItemDataInline,
+    ]
+
+
+@admin.register(InventoryIdSchema)
+class InventoryIdSchemaAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'note',),
+        }),
+    )
+
+
+class OverlayItemInline(CommonInline):
+    model = OverlayItem
+
+
+@admin.register(Overlay)
+class OverlayAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'note',),
+        }),
+        ("Status", {
+            'fields': ('active',),
+        }),
+        ("Properties", {
+            'fields': ('parent',),
+        }),
+    )
+    inlines = [
+        OverlayItemInline,
+    ]
