194aee5f31dcd253fe2f36dbe147c7674a739aaf
Moritz 'e1mo' Fromm <git@e1mo.de>
2021-05-31 19:31:21 +0200

    Add more token related tests
    - test_token_bad_password
    - test_token_delete
      - Currently fails due to missing / in the endpoint => fix in next
        commit
    - test_token_new
      - Limited, see #79
    - test_token_expiry
      - Taking 1.1 seconds (due to a short wait before the token expires)
      - Paralel tests could reduce that time but currently they do not work

--- src/tests/authentication/test_token.py
+++ src/tests/authentication/test_token.py
@@ -1,5 +1,7 @@
 import json
-from datetime import datetime
+import uuid
+from datetime import datetime, timedelta
+from time import sleep
 from typing import Optional
 
 from django.contrib.auth import get_user_model
@@ -9,20 +11,34 @@ from django.test import TestCase
 from chaosinventory.authentication.models import Token, User
 
 
-def get_db_token(key):
+def get_db_token(key) -> Token:
     return Token.objects.get(key=key)
+
+
+def create_user(**kwargs) -> User:
+    password = None
+    if 'password' in kwargs:
+        password = kwargs.pop('password')
+
+    default_user_data = {
+        'username': uuid.uuid4(),
+        'first_name': 'Demo',
+        'last_name': 'User',
+        'email': 'user@example.org',
+    }
+    user: User = get_user_model().objects.create(**{**default_user_data, **kwargs})
+
+    if password is not None:
+        user.set_password(password)
+        user.save()
+
+    return user
 
 
 class TokenTestCase(TestCase):
     def setUp(self) -> None:
         self.user_password: str = 'password'
-        self.user: User = get_user_model()()
-        self.user.username = 'test'
-        self.user.first_name = 'Demo'
-        self.user.last_name = 'User'
-        self.user.email = 'user@example.org'
-        self.user.set_password(self.user_password)
-        self.user.save()
+        self.user = create_user(password=self.user_password)
 
     def obtain_token(self, username: str = None, password: str = None, application: str = None,
                      expiring: Optional[datetime] = None, renewable: bool = True):
@@ -32,7 +48,6 @@ class TokenTestCase(TestCase):
             'application': self.__class__.__name__ if application is None else application,
             'renewable': renewable,
         }
-
         if expiring is not None:
             payload['expiring'] = expiring
 
@@ -74,6 +89,12 @@ class TokenTestCase(TestCase):
                 payload,
                 **headers,
             )
+        elif method == 'delete':
+            response = self.client.delete(
+                path,
+                payload,
+                **headers,
+            )
         else:
             raise ValueError('method mus be either get or post')
 
@@ -83,10 +104,11 @@ class TokenTestCase(TestCase):
         else:
             return response, response.content
 
-    def validate_token_against_api(self, token):
+    def validate_token_against_api(self, token: Optional[str] = None, expected_status_code: Optional[int] = 200):
         response, response_content = self.api_call('/api/authentication/token/', token=token)
-        self.assertEqual(response.status_code, 200)
-        return response
+        if expected_status_code is not None:
+            self.assertEqual(response.status_code, expected_status_code)
+        return response, response_content
 
     def test_obtain_token_json(self):
         response = self.obtain_token()
@@ -94,6 +116,31 @@ class TokenTestCase(TestCase):
         self.assertEqual(response['content-type'], 'application/json')
         token = self.assertValidJson(response.content)
         self.assertTrue('token' in token)
+
+    def test_token_bad_password(self):
+        response = self.obtain_token(password='')
+        self.assertEqual(response.status_code, 400)
+        self.assertValidJson(response.content)
+
+    def test_inactive_user_token(self):
+        user = create_user(
+            password=self.user_password,
+        )
+        token = self.get_token(
+            username=user.username,
+            password=self.user_password,
+        )
+        user.is_active = False
+        user.save()
+        self.validate_token_against_api(token, 401)
+
+    def test_token_expiry(self):
+        expiring = datetime.now() + timedelta(seconds=1)
+        token = self.get_token(expiring=expiring)
+        self.validate_token_against_api(token)
+        sleep(1.1)
+        response, content = self.validate_token_against_api(token, 401)
+        self.assertEqual(content['detail'], 'Expired token.')
 
     def test_user_token_in_db(self):
         token = self.get_token()
@@ -122,22 +169,53 @@ class TokenTestCase(TestCase):
         self.validate_token_renewal(False)
 
     def test_token_api_auth(self):
-        self.assertEqual(
-            self.api_call('/api/authentication/token/')[0].status_code,
-            401,
-        )
+        self.validate_token_against_api(None, 401)
+        self.validate_token_against_api('invalid', 401)
         token = self.get_token()
-        self.assertEqual(
-            self.api_call(
-                '/api/authentication/token/',
-                token='invalid',
-            )[0].status_code,
-            401
+        self.validate_token_against_api(token, 200)
+
+    def test_token_new(self):
+        token = self.get_token()
+        response, content = self.api_call(
+            '/api/authentication/token/',
+            token=token,
+            method='post',
+            payload={
+                'application': self.__class__.__name__,
+            }
         )
-        self.assertEqual(
-            self.api_call(
-                '/api/authentication/token/',
-                token=token,
-            )[0].status_code,
-            200,
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('token' in content)
+
+        # Disabled until #79 is fixed
+        #
+        # token = self.get_token(renewable=False)
+        # response, content = self.api_call(
+        #     '/api/authentication/token/',
+        #     token=token,
+        #     method='post',
+        #     payload={
+        #         'application': self.__class__.__name__,
+        #     }
+        # )
+        # self.assertEqual(response.status_code, 403)
+        # self.assertFalse('token' in content)
+
+    def test_token_delete(self):
+        token = self.get_token()
+        db_token = get_db_token(token)
+        random_uuid = str(uuid.uuid4())
+        response, content = self.api_call(
+            f'/api/authentication/token/{random_uuid}/',
+            token=token,
+            method='delete',
         )
+        self.assertEqual(response.status_code, 404)
+        response, content = self.api_call(
+            f'/api/authentication/token/{db_token.pk}/',
+            token=token,
+            method='delete',
+        )
+        self.assertEqual(response.status_code, 200)
+        with self.assertRaises(ObjectDoesNotExist):
+            get_db_token(token)
