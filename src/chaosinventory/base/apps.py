bb767907526e8d0e1395e2e4b1de354c66f846f5
Moritz 'e1mo' Fromm <git@e1mo.de>
2021-06-11 18:51:10 +0200

    Implement djangos new default_auto_field
    Using BigAutoField does not brak annything for us (going from a
    integer guaranteed 64 bit integer), thus I implemented the new django
    default value instead of keeping the old one.
    
    Ref:
    <https://docs.djangoproject.com/en/3.2/releases/3.2/#customizing-type-of-auto-created-primary-keys>

--- src/chaosinventory/base/apps.py
+++ src/chaosinventory/base/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class BaseConfig(AppConfig):
     # See https://stackoverflow.com/a/67057826/11249686
     name = 'chaosinventory.base'
+    # Introduced in the Django3.2 update, BigAutoField is the
+    # new default and should not break anything for us
+    default_auto_field = 'django.db.models.BigAutoField'
