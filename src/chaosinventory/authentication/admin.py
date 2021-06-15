62eaddb8784629b6ef27a4e2d227808584d90b3b
Moritz 'e1mo' Fromm <git@e1mo.de>
2021-06-15 16:47:57 +0200

    Add OIDCApplications to admin site

--- src/chaosinventory/authentication/admin.py
+++ src/chaosinventory/authentication/admin.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 # Register your models here.
 from .models import Token, User
+from .models.oidc import OIDCApplication
 
 
 @admin.register(User)
@@ -24,3 +25,41 @@ class TokenAdmin(admin.ModelAdmin):
     fields = ('user',)
     list_display = ('id', 'key', 'user', 'created', 'application', 'expiring', 'renewable')
     actions = None
+
+
+@admin.register(OIDCApplication)
+class OIDCApplicationAdmin(admin.ModelAdmin):
+    fields = (
+        'client_id',
+        'name',
+        'description',
+        'access_type',
+        'default_redirect_uri',
+        'redirect_uris',
+        'created',
+    )
+    readonly_fields = (
+        'client_id',
+        'created',
+    )
+    list_display = (
+        'name',
+        'access_type',
+        'created',
+    )
+    search_fields = (
+        'client_id',
+        'name',
+        'description',
+    )
+    list_filter = (
+        'access_type',
+        'created',
+    )
+
+    def has_add_permission(self, request):
+        """
+        We do not want to create OIDCApplications from the admin interface.
+        Use the create_oidc_application management command instead.
+        """
+        return False
