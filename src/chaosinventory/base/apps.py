ed50b9a47c03fbc00c32f258b319c0f764a51eab
Moritz <git@e1mo.de>
2021-08-06 22:15:40 +0200

    Update to Django 3.2 (#81)
    * Raise django version
    
    * Implement Django3.2 app discovery
    
    After upgrading to Django3.2 a `django.core.exceptions.ImproperlyConfigured`
    exception was raised, this is now fixed as described in this
    stackoverflow:
    
    https://stackoverflow.com/a/67057826/11249686
    
    * Implement djangos new default_auto_field
    
    Using BigAutoField does not brak annything for us (going from a
    integer guaranteed 64 bit integer), thus I implemented the new django
    default value instead of keeping the old one.
    
    Ref:
    <https://docs.djangoproject.com/en/3.2/releases/3.2/#customizing-type-of-auto-created-primary-keys>
    
    * Raise minimum Python version to 3.6
    
    Django 3.2 only supports Python >=3.6
    
    Also added python 3.9 tag to the setup.cfg and raised the django version
    to 3.2 there to.
    
    Ref: <https://docs.djangoproject.com/en/3.2/releases/3.2/#python-compatibility>

--- src/chaosinventory/base/apps.py
+++ src/chaosinventory/base/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class BaseConfig(AppConfig):
-    name = 'base'
+    # See https://stackoverflow.com/a/67057826/11249686
+    name = 'chaosinventory.base'
+    # Introduced in the Django3.2 update, BigAutoField is the
+    # new default and should not break anything for us
+    default_auto_field = 'django.db.models.BigAutoField'
